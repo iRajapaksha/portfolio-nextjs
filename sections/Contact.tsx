e452c8337796fa2ad513ed992bea19c8a8170cbc
iRajapaksha <nayanajith.ishara2541@gmail.com>
2024-07-12 13:07:43 +0530

    contact form

--- sections/Contact.tsx
+++ sections/Contact.tsx
@@ -1,9 +1,14 @@
 import ContactCard from '@/components/card/ContactCard'
 import Heading from '@/components/heading/Heading'
+import Button from '@/components/ui/Button'
 import Card from '@/components/ui/Card'
+import Input from '@/components/ui/Input'
+import TextArea from '@/components/ui/TextArea'
 import React from 'react'
-import { FaPhoneVolume } from 'react-icons/fa'
-import { MdEmail } from 'react-icons/md'
+import { FaPhoneVolume, FaUser } from 'react-icons/fa'
+import { FaMessage } from 'react-icons/fa6'
+import { MdEmail, MdSubject } from 'react-icons/md'
+import { SiMinutemailer } from 'react-icons/si'
 
 
 
@@ -13,6 +18,7 @@ function ContactSection() {
         <Heading number='03' title1='Contact' title2='Me'/>
         <Card>
             <div className="grid gap-8 grid-cols-1 lg:grid-cols-3">
+                {/**Contact cards */}
                 <div className="flex flex-col gap-8">
                     <ContactCard
                     title='Call us directly'
@@ -26,6 +32,23 @@ function ContactSection() {
                     text='rajapakshaani@gmail.com'
                     btnText='Email '
                     />
+                </div>
+                {/**Contact form */}
+                <div className="lg:col-span-2 bg-secondary-background border border-border rounded-lg space-y-6 relative overflow-hidden py-5 px-[25px] shadow-md ">
+                    <div className="flex flex-col lg:flex-row items-center justify-between mb-4 gap-8  ">
+                        <Input type='text' placeholder='Name' icon={<FaUser/>}/>
+                        <Input type='email' placeholder='Email Address' icon={<MdEmail/>}/>
+
+                    </div>
+                    <div className="flex  items-center justify-between mb-4 gap-8  ">
+                        <Input type='text' placeholder='Subject' icon={<MdSubject/>}/>
+                    </div>
+                    <TextArea placeholder='Message' icon={<FaMessage/>}/>
+                    <div className="w-full flex justify-end">
+                        <Button className='!w-44 !py-3 !text-xl'>
+                            Send <SiMinutemailer/>
+                        </Button>
+                    </div>
                 </div>
             </div>
             
